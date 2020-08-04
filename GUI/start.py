eab5170b2b0e998f652e3e9ed1670f6346c08db2
Jitensid <46622106+Jitensid@users.noreply.github.com>
2020-08-04 17:00:28 +0530

    FINAL GUI PYTHON FILE

--- GUI/start.py
+++ GUI/start.py
@@ -4,6 +4,8 @@ from tkinter import filedialog
 
 path_dictionary = dict()
 
+parameter_dict = dict()
+
 # @eel.expose
 # def selectFolder(num):
 
@@ -37,6 +39,20 @@ def selectFolder(num):
 	root.destroy()
 
 	return str(directory_path)
+
+@eel.expose
+def fetch_all_parameters(input_filter,input_spectral,input_magnitude,input_photometric,output_filter,output_photometric):
+
+	# print("RECEIVED ALL PARAMETERS ")
+
+	print(input_filter,input_spectral,input_magnitude,input_photometric,output_filter,output_photometric)
+
+	parameter_dict["input_filter"] = input_filter
+	parameter_dict["input_spectral"] = input_spectral
+	parameter_dict["input_magnitude"] = input_magnitude
+	parameter_dict["input_photometric"] = input_photometric
+	parameter_dict["output_filter"] = output_filter
+	parameter_dict["output_photometric"] = output_photometric
 
 eel.init('web')
 eel.start('main.html')
